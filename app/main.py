4a8ba484bf55ca4ca17ac8d352af1f96752b537f
Mank <91036546+mankkk208@users.noreply.github.com>
2025-02-24 17:01:19 +0700

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -8,21 +8,29 @@ from app.routers.GCS_router import gcs_router
 
 app = FastAPI()
 
+# Cấu hình CORS chi tiết hơn
+origins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Cho phép tất cả các domain. Bạn có thể thay "*" bằng các domain cụ thể nếu cần.
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Cho phép tất cả các phương thức HTTP
-    allow_headers=["*"],  # Cho phép tất cả các headers
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
 )
 
-# Registering the routers for CNN and YOLO
+# Registering the routers
 app.include_router(cnn_router, prefix="/cnn")
 app.include_router(yolo_router, prefix="/yolo")
 app.include_router(gemini_router, prefix="/gemini")
 app.include_router(gpt_router, prefix="/gpt")
 app.include_router(gcs_router, prefix="/gcs")
-
 
 @app.get("/")
 async def root():
